8890e837b96e26005a3dacf335b722d261d7324c
Himanshu-0901 <himanshukumawat2004j@gmail.com>
2025-09-05 17:46:56 +0530

    Add the CLI

--- Backend/shortener.js
+++ Backend/shortener.js
@@ -2,20 +2,20 @@
 import axios from "axios";
 import chalk from "chalk";
 
-const inputUrl = process.argv[2]; // take URL from CLI
+const inputUrl = process.argv[2]; 
 
 if (!inputUrl) {
-  console.log(chalk.red("❌ Please provide a URL!"));
+  console.log(chalk.red("Please provide a URL!"));
   process.exit(1);
 }
 
 async function shortenUrl() {
   try {
-    const res = await axios.post("http://localhost:3000/shorten", {
+    const res = await axios.post(`${process.env.APP_URL}/shorten`, {
       url: inputUrl,
     });
 
-    console.log(chalk.green("✅ Short URL:"), chalk.blue(res.data.shortUrl));
+    console.log(chalk.green("Short URL:"), chalk.blue(res.data.shortUrl));
   } catch (err) {
     console.error(chalk.red("⚠️ Error:"), err.message);
   }
